f9b9616e3e62e0a33c7d63ad2680eb664a0a4cde
Nguyen Cong Huong <huongcaoha1994@gmail.com>
2023-10-04 00:17:59 +0700

    push product-detail.php

--- SRC/core/controller/product-detail.php
+++ SRC/core/controller/product-detail.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    <?php include("../../Hieu_demo1/header.php") ; ?>
     <h1>Product Detail</h1>
     <?php
     include("../model/database.php");
@@ -17,7 +18,7 @@
                 $row = $result_product->fetch_assoc() ;
                 ?>
                     <div>
-                        <img src="<?php echo $row['image'] ; ?>" alt="<?php echo $row['product_name'] ; ?>">
+                        <img src="../../assets/img<?php echo $row['image'] ; ?>" alt="" width="300" height="300">
                         <h2><?php echo $row['product_name'] ; ?></h2>
                         <h3>Price : <?php echo $row['price'] ; ?></h3>
                         <h3>Color : <?php echo $row['color'] ; ?></h3>
@@ -37,6 +38,33 @@
                 <?php
             }
         }
+    ?>
+
+    <?php 
+    session_start();
+    if(isset($_POST['add_to_cart'])){
+        $product_id = htmlspecialchars($_POST['product_id']);
+        $quantity = htmlspecialchars($_POST['quantity']);
+        $get_product = "select * from products where product_id = $product_id ;" ;
+        $result_product = $connect->query($get_product);
+        if($result_product->num_rows > 0 ){
+            $row = $result_product->fetch_assoc();
+            $product_id = $row['product_id'] ;
+            $price = $row['price'] ;
+            $total_money = $quantity * $price ;
+            $user_id = $_SESSION['user_id'] ;
+            $insert_cart = "insert into carts (product_id,price,quantity,total_money,user_id)
+            values ($product_id,$price,$quantity,$total_money,$user_id]) ;" ;
+            if($insert_cart){
+                echo "<script> alert ('thêm giỏ hàng thành công')</script>" ;
+                header("../../view/home.php");
+                exit ;
+            }
+            else {
+                echo "<script> alert ('xin vui lòng thử lại !')</script>" ;
+            }
+        }
+    }
     ?>
 </body>
 </html>
