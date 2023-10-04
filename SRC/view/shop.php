812e78126a2128c65e18c5fb5762e31650496adb
Nguyễn Thành Hiếu <hieunguyenthanh165@gmail.com>
2023-10-04 23:47:22 +0700

    1

--- SRC/view/shop.php
+++ SRC/view/shop.php
@@ -14,13 +14,15 @@
     <!-- breadcrumb -->
     <div class="container py-3">
         <div class="row">
-            <div class="col-12">
+            <div class="col-6">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#" class="display-6">Home</a></li>
                         <li class="breadcrumb-item active display-6" aria-current="page">Shop</li>
                     </ol>
                 </nav>
+            </div>
+            <div class="col-6">
             </div>
         </div>
     </div>
