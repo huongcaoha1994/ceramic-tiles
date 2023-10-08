00873f959a3ad9c55286a47be363b178ad113d9e
nguyenvantruc <truc.nv.2296@aptechleaarning.edu.vn>
2023-10-09 01:11:43 +0700

    Update contact & AboutUs

--- SRC/view/contact.php
+++ SRC/view/contact.php
@@ -1,83 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="contact.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="contact.css">
+    <!--boosstrap 5 icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
+
 <body>
-    <div class="wrapper">
-        <div class="header"> 
-
-        </div>
-        <div class="Notification">
-            <h3>Contect</h3>
-            <p>Thank you for your interest in Nanomart. We want to hear from you, if you have questions, comments or answers. Please contact us using the following information.</p>
-        </div>
-            
-         <div class="main-content">
-            <div class="content">
-                <h3>CÔNG TY TNHH TITLE LUXURY</h3> 
-
-                <ul>
-                    <li><p>NumBer: <b>0915 324 128</b></p></li>
-                    <li><p>Collaborators: <b>0979 697 715 | 0979 957 815</b></p></li>
-                    <li><p>Email: Nguyenvantruc206@gmail.com</p></li>
-                    <li><p> Website: https://tilesLuxury.vn</p></li>
-                </ul>
-            </div>
-            <div class="sidebar">
-                <form >
-                    <h3>Email liên hệ</h3>
-                    <label for="name">Your Name:</label>
-                    <input type="text" id="name" name="name" required>
-
-                    <label for="name">Number:</label>
-                    <input type="text" id="name" name="name" required>
-
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" required>
-
-                    <label for="message">Message Content:</label>
-                    <textarea id="message" name="message" required></textarea>
-
-                    <input type="submit" value="Submit">
-                </form>
-        
-            </div>
-            </div>
-        <div class="footer">
-            <div class="footer-content">
-                <div class="mailing">
-                    <ul>
-                        <b>Công ty TNHH Title LUXURY</b>
-                        <li> 6B Nguyễn Thành Ý, Phường Dakao, Quận 1, Hà Nội</li>
-                        <li>Số 13, Đường số 1, Cityland Park Hills, Quận Gò Vấp, Hà nội</li>
-                        <p><b>Hotline:</b>0964.625.230</p>
-                    </ul>
-                </div>
-                <div class="support">
-                    <ul>
-                        <b>Hỗ Trợ</b>
-                        <li>Chính sách bảo mật</li>
-                        <li>Điều khoản sử dụng</li>
-                        <li>Điều kiện giao dịch </li>
-                        <li>Phương thức thanh toán</li>
-                        <li>vận chuyển & Giao hàng</li>
-                    </ul>
-                </div>
-
-
+    <?php
+    include("header.php")
+    ?>
+    <img src="../assets/img/homepage/anhbia.jpeg" alt="" class="img-fluid w-100">
+    <div class="container1">
+        <div class="row">
+            <div class="header">
+                <h3>Contact</h3>
+                <p>Thank you for your interest in Nanomart. We want to hear from you, if you have questions, comments or answers. Please contact us using the following information.</p>
             </div>
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div calss="container">
+        <div class="row">
+            <div class="col-md-7">
+                <form class="main">
+                    <h2>Git in touch</h2>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Your Name</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Name">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Phone Number">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Your Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            
+
+            <div class="col-md-5">
+                <h4>Contact Us</h4>
+                <div class="mt-5">
+                    <div class="d-flex">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <p>Address :19 Lê Thanh Nghị , Bạch Mai , Hai Bà Trưng , Hà Nội </p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="bi bi-telephone-fill"></i>
+                        <p>Hotline : 0964625320 </p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="bi bi-envelope"></i>
+                        <a href="mailto:Nguyenvantruc206@gmail.com">Nguyenvantruc206@gmail.com</a>
+                       
+                    </div>
+                    <div class="d-flex">
+                        <i class="bi bi-browser-chrome"></i>
+                        <p>  Website: https:/Ceramic-Tiles.vn</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="col-12 mb-6">
+                <form class="google">
+                    <h5>Our Location</h5>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.940115968208!2d105.8494022744659!3d21.035081980615843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abbf5c6e7ce3%3A0xcfa017611623993c!2zMTkwMCBMZSBUaMOpw6J0cmU!5e0!3m2!1svi!2s!4v1695882844641!5m2!1svi!2s"
+                        width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </form>
+            </div>
+    </div>
+
+
+    <?php
+    include("footer.php")
+    ?>
 </body>
+
 </html>
