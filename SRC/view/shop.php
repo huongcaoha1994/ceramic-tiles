d0b093dd6d7411223878a066438cdb1084e388e0
Nguyễn Thành Hiếu <hieunguyenthanh165@gmail.com>
2023-10-09 20:50:56 +0700

    update shop offcanvas

--- SRC/view/shop.php
+++ SRC/view/shop.php
@@ -93,13 +93,26 @@
       <div class="col-12 col-md-4">
         <h1 class="text-center d-md-none">Store</h1>
       </div>
-      <div class="col-12 text-center text-md-end col-md-4">
+      <div class="col-2 d-md-none">
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasStore" aria-controls="offcanvasStore">
+        <i class="fa-solid fa-filter"></i>
+        </button>
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasStore" aria-labelledby="offcanvasStoreLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasStoreLabel">Filter</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+          </div>
+        </div>
+      </div>
+      <div class="col-10 text-center text-md-end col-md-4">
         <form action="shop.php" method="POST" class="d-flex">
           <input type="text" name="rs_search" class="form-control rounded">
           <input type="submit" name="search" value="search" class="btn btn-outline-primary">
         </form>
-
       </div>
+      
     </div>
   </div>
   <!-- Heading -->
@@ -107,81 +120,82 @@
   <!-- Main content -->
   <div class="container">
     <div class="row">
-      <div class="d-none col-lg-3 d-lg-block border rounded-1 p-3">
+      <div class="d-none col-lg-3 d-lg-block border rounded-1 p-3 filter_offcanvas">
+
         <h4>Bộ lọc sản phẩm</h4>
-        <h2>color</h2>
-        <label for="color">black</label>
-        <input type="radio" name="color" value="black" onclick="filter_color('black')">
-        <br>
-        <label for="color">white</label>
-        <input type="radio" name="color" value="white" onclick="filter_color('white')">
-        <br>
-        <label for="color">yellow</label>
-        <input type="radio" name="color" value="yellow" onclick="filter_color('yellow')">
-        <br>
-        <label for="color">orange</label>
-        <input type="radio" name="color" value="orange" onclick="filter_color('orange')">
-        <br>
-        <label for="color">red</label>
-        <input type="radio" name="color" value="red" onclick="filter_color('red')">
-        <br>
-        <label for="color">brown</label>
-        <input type="radio" name="color" value="brown" onclick="filter_color('brown')">
-        <br>
-        <label for="color">pink</label>
-        <input type="radio" name="color" value="pink" onclick="filter_color('pink')">
-        <br>
-        <label for="color">All</label>
+        <h2>Color</h2>
         <input type="radio" name="color" value="" onclick="filter_color('')">
+        <label for="color">All</label>
+        <br>
+        <input type="radio" name="color" value="white" onclick="filter_color('white')">
+        <label for="color">White</label>
+        <br>
+        <input type="radio" name="color" value="yellow" onclick="filter_color('yellow')">
+        <label for="color">Yellow</label>
+        <br>
+        <input type="radio" name="color" value="orange" onclick="filter_color('orange')">
+        <label for="color">Orange</label>
+        <br>
+        <input type="radio" name="color" value="red" onclick="filter_color('red')">
+        <label for="color">Red</label>
+        <br>
+        <input type="radio" name="color" value="brown" onclick="filter_color('brown')">
+        <label for="color">Brown</label>
+        <br>
+        <input type="radio" name="color" value="pink" onclick="filter_color('pink')">
+        <label for="color">Pink</label>
+        <br>
+        <input type="radio" name="color" value="black" onclick="filter_color('black')">
+        <label for="color">Black</label>
         <br>
 
-        <h2>brand</h2>
-        <label for="brand">Viglacera</label>
+        <h2>Brand</h2>
         <input type="radio" name="brand" value="Viglacera" onclick="filter_brand('Viglacera')">
+        <label for="brand">Viglacera</label>
         <br>
-        <label for="brand">Bach Ma Tiles</label>
         <input type="radio" name="brand" value="Bach Ma Tiles" onclick="filter_brand('Bach Ma Tiles')">
+        <label for="brand">Bach Ma Tiles</label>
         <br>
-        <label for="brand">Chinese Tiles</label>
         <input type="radio" name="brand" value="Chinese Tiles" onclick="filter_brand('Chinese Tiles')">
+        <label for="brand">Chinese Tiles</label>
         <br>
-        <label for="brand">Dong Tam Brick</label>
         <input type="radio" name="brand" value="Dong Tam Brick" onclick="filter_brand('Dong Tam Brick')">
+        <label for="brand">Dong Tam Brick</label>
         <br>
-        <label for="brand">Eurotile</label>
         <input type="radio" name="brand" value="Eurotile" onclick="filter_brand('Eurotile')">
+        <label for="brand">Eurotile</label>
         <br>
-        <label for="brand">All</label>
         <input type="radio" name="brand" value="" onclick="filter_brand('')">
+        <label for="brand">All</label>
         <br>
 
-        <h2>size</h2>
-        <label for="size">60x60</label>
+        <h2>Size</h2>
         <input type="radio" name="size" value="60x60" onclick="filter_size('60x60')">
+        <label for="size">60x60</label>
         <br>
-        <label for="size">80x80</label>
         <input type="radio" name="size" value="80x80" onclick="filter_size('80x80')">
+        <label for="size">80x80</label>
         <br>
-        <label for="size">45x45</label>
         <input type="radio" name="size" value="45x45" onclick="filter_size('45x45')">
+        <label for="size">45x45</label>
         <br>
-        <label for="size">40x80</label>
         <input type="radio" name="size" value="40x80" onclick="filter_size('40x80')">
+        <label for="size">40x80</label>
         <br>
-        <label for="size">40x40</label>
         <input type="radio" name="size" value="40x40" onclick="filter_size('40x40')">
+        <label for="size">40x40</label>
         <br>
-        <label for="size">25x40</label>
         <input type="radio" name="size" value="25x40" onclick="filter_size('25x40')">
+        <label for="size">25x40</label>
         <br>
-        <label for="size">30x60</label>
         <input type="radio" name="size" value="30x60" onclick="filter_size('30x60')">
+        <label for="size">30x60</label>
         <br>
-        <label for="size">30x90</label>
         <input type="radio" name="size" value="30x90" onclick="filter_size('30x90')">
+        <label for="size">30x90</label>
         <br>
-        <label for="size">All</label>
         <input type="radio" name="size" value="" onclick="filter_size('')">
+        <label for="size">All</label>
         <br>
 
       </div>
