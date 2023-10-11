f8346d5c7d38804eaef2f83d229cc63535b832e5
Nguyenvantruc <truc.nv.2296@aptechlearning.edu.vn>
2023-10-11 20:57:01 +0700

    update_blog

--- SRC/core/model/blog_detail.php
+++ SRC/core/model/blog_detail.php
@@ -7,14 +7,7 @@ if (isset($_GET['blog_id'])) {
     if ($result_blog->num_rows > 0) {
         $row = $result_blog->fetch_assoc();
 ?>
-        <div>
-            <img src="../<?php echo $row['image']; ?>" alt="<?php echo $row['title']; ?>">
-            <h3><?php echo $row['title']; ?></h3>
-            <h4><?php echo $row['content']; ?></h4>
-            <h5>Người viết bài :<?php echo $row['author']; ?></h5>
-            <h5>Ngày đăng :<?php echo $row['created_date']; ?></h5>
-        </div>
-        <p>__________________________________________________________________________________________________________________</p>
+        
 <?php
 
     } else {
@@ -22,3 +15,34 @@ if (isset($_GET['blog_id'])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../assets/css/blog_detail.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+    
+</head>
+<body>
+        <div class="card mb-3">
+                <img src="../<?php echo $row['image']; ?>" alt="<?php echo $row['title']; ?>">
+            <div class="card-body">
+                <h3 class="card-title"><?php echo $row['title']; ?></h3>
+                <p class="card-text"><?php echo $row['content']; ?></p>
+                <p class="card-text"><small class="text-muted">Người viết bài :<?php echo $row['author']; ?></small></p>
+                <p class="card-text"><small class="text-muted">Ngày đăng :<?php echo $row['created_date']; ?></small></p>
+
+            </div>
+        </div>
+            
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
