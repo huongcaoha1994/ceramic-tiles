10e3f4d1921fef63a93c143d10b6f151af34908f
Nguyễn Thành Hiếu <hieunguyenthanh165@gmail.com>
2023-10-13 20:59:16 +0700

    css user

--- SRC/view/user.php
+++ SRC/view/user.php
@@ -30,86 +30,96 @@ $result = $connect->query($sql);
 </head>
 
 <body>
-    <table>
-        <tr>
-            <th>Created at</th>
-            <th>Orderer's information</th>
-            <th>Receiver's information</th>
-            <th>Status</th>
-            <th>Total amout</th>
-            <th>View detail</th>
-        </tr>
-        <?php foreach ($result as $each): ?>
+    <div class="container">
+        <table class="table table-responsive">
             <tr>
-                <td>
-                    <?php echo $each['created_at'] ?>
-                </td>
-
-
-                <td>
-                    <?php echo $each['full_name'] ?>
-                    <?php echo $each['phone'] ?>
-                    <?php echo $each['user_address'] ?>
-                </td>
-
-
-                <td>
-                    <?php echo $each['name_receiver'] ?>
-                    <?php echo $each['phone_receiver'] ?>
-                    <?php echo $each['receiver_address'] ?>
-                </td>
-                <td>
-                    <?php 
-                    switch ($each['status']) {
-                        case 0:
-                            echo "Pending";
-                            break; 
-                        case 1:
-                            echo "Order is being delivered";
-                            break;
-                        case 2:
-                            echo "Received";
-                            break;
-                        }
-                    ?>
-                </td>
-                <td>
-                    <?php echo $each['total_price']?>
-                </td>
-                <td>
-                    <!-- <a href="order_detail.php?order_id=<?php echo $each['order_id']?>">View detail</a> -->
-                </td>
+                <th>Created at</th>
+                <th>Orderer's information</th>
+                <th>Receiver's information</th>
+                <th>Status</th>
+                <th>Total amout</th>
+                <th>View detail</th>
             </tr>
+            <?php foreach ($result as $each) : ?>
+                <tr>
+                    <td>
+                        <?php echo $each['created_at'] ?>
+                    </td>
+                    <td>
+                        <?php echo $each['full_name'] ?>
+                        <?php echo $each['phone'] ?>
+                        <?php echo $each['user_address'] ?>
+                    </td>
 
-    </table>
 
-    <?php 
-    $order_id = $each['order_id'];
-    $sql = "select * from order_product
+                    <td>
+                        <?php echo $each['name_receiver'] ?>
+                        <?php echo $each['phone_receiver'] ?>
+                        <?php echo $each['receiver_address'] ?>
+                    </td>
+                    <td>
+                        <?php
+                        switch ($each['status']) {
+                            case 0:
+                                echo "Pending";
+                                break;
+                            case 1:
+                                echo "Order is being delivered";
+                                break;
+                            case 2:
+                                echo "Received";
+                                break;
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <?php echo $each['total_price'] ?>
+                    </td>
+                    <td>
+                        <!-- <a href="order_detail.php?order_id=<?php echo $each['order_id'] ?>">View detail</a> -->
+                    </td>
+                </tr>
+        </table>
+
+        <?php
+                $order_id = $each['order_id'];
+                $sql = "select * from order_product
     join products on products.product_id = order_product.product_id
     where order_id = $order_id";
 
-    $result = $connect->query($sql);
-    foreach ($result as $each):
-        $product_id = $each['product_id'];
-            $sql = "select * from products 
+                $result = $connect->query($sql);
+                foreach ($result as $each) :
+                    $product_id = $each['product_id'];
+                    $sql = "select * from products 
                 join order_product 
                 on products.product_id = order_product.product_id
                 where products.product_id = $product_id";
-            $result = $connect->query($sql);
-            $each = mysqli_fetch_array($result);
-            ?>
-            <img src="../assets/img/<?php echo $each['image']; ?>" alt="Product Image"
-                        style="max-width: 100px;">
-            
-            <p><?php echo $each['product_name']; ?></p>
-            <p><?php echo $each['price']; ?></p>
-            <p><?php echo $each['quantity']; ?></p>
-            <p><?php echo $each['price'] * $each['quantity']; ?></p>
-            <?php
-        endforeach; 
-    ?>
-            <?php endforeach ?>
+                    $result = $connect->query($sql);
+                    $each = mysqli_fetch_array($result);
+        ?>
+            <div class="container">
+                <div class="row py-2">
+                    <div class="col-10 col-lg-6 d-flex">
+                        <img src="../assets/img/<?php echo $each['image']; ?>" alt="Product Image" style="max-width: 50px;">
+                        <span class="px-2 h6"><?php echo $each['product_name']; ?></span>
+                    </div>
+                    <div class="col-2 col-lg-2 ">
+                        <p class="h6"><?php echo $each['price']; ?>$</p>
+                    </div>
+                    <div class="col-6 col-lg-2 ">
+                        <p>Quantity: <?php echo $each['quantity']; ?></p>
+                    </div>
+                    <div class="col-6 col-lg-2  text-end">
+                        <p>Total: <?php echo $each['price'] * $each['quantity']; ?>$</p>
+                    </div>
+                </div>
+            </div>
+
+        <?php
+                endforeach;
+        ?>
+    <?php endforeach ?>
+    </div>
 
 </body>
 
