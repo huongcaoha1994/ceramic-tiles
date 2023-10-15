82867e132d661cde986ef13f7c1fba16e7915eeb
Trinh Van Vy <trinhvanvy2000@gmail.com>
2023-10-15 22:43:55 +0700

    fix link home admin

--- SRC/view/order.php
+++ SRC/view/order.php
@@ -34,7 +34,7 @@ $result = $connect->query($sql);
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-warning">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="#"
+                    <a href="../view/main.php"
                         class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <img src="../assets/img/homepage/logo.png" alt="" height="70">
                     </a>
