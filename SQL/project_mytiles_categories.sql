205a89485220fc8309e9241893cae55de0f0a19a
Nguyen Cong Huong <huongcaoha1994@gmail.com>
2023-10-15 21:41:28 +0700

    update table categories

--- SQL/project_mytiles_categories.sql
+++ SQL/project_mytiles_categories.sql
@@ -23,8 +23,8 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
-  `categories_id` int NOT NULL,
-  `categories_name` varchar(50) NOT NULL
+  `category_id` int NOT NULL,
+  `category_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-15  0:09:55
+-- Dump completed on 2023-10-15 21:39:59
