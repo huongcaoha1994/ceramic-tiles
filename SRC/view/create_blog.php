503ce5382780d2b244b55866725a6d342dd6c114
Nguyen Cong Huong <huongcaoha1994@gmail.com>
2023-10-19 00:44:49 +0700

    fix

--- SRC/view/create_blog.php
+++ SRC/view/create_blog.php
@@ -9,7 +9,20 @@
 <body>
     <?php
         include("header.php");
+    
     ?>
+    <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="./main.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Expert Corner</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
     <div class="container">
 
    
