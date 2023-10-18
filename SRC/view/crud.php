3f41dfb095f67a0803ca892b584864aafd4feaca
Trinh Van Vy <trinhvanvy2000@gmail.com>
2023-10-18 23:54:11 +0700

    fix 2

--- SRC/view/crud.php
+++ SRC/view/crud.php
@@ -14,13 +14,6 @@ require '../core/model/database.php';
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="../assets/css/crud.css">
         <title>CRUD</title>
-        <style>
-            .pagination {
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-}
-        </style>
     </head>
     <body>
     <?php 
